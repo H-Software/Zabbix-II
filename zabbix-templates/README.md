edbbcbeabfc1397fe012ca085e757312e66c4b8d
root <root@huhu-centos-6-64bit.(none)>
2013-10-30 12:22:02 +0100

    improved script a params in template linux-disk-io-stats

--- zabbix-templates/README.md
+++ zabbix-templates/README.md
@@ -44,6 +44,14 @@ import xml file (zbx_templates_rabbitmq-server.xml) as zabbix template
 Linux disk io stats
 ------
 
+tested on:
+
+Centos 6.x x86_64
+Ubuntu LTS 12.04 x86_64
+
+instalation
+-------
+
 :: on monitored host
 
 1. copy files from folder "usr-local-bin" to /usr/local/bin
@@ -58,6 +66,16 @@ https://github.com/czhujer/puppet-zabbixagent
 :: on server
 
 import xml file (zbx_templates_linux_disk_io_stats.xml) as zabbix template
+
+Changelog
+-------
+
+v1.1 - 2013/10/30
+
+updated parse script and zabbix userparameters
+ -- Now, the parameters and their sequence are looking directly (iostat in Ubuntu writes more information/parameters)
+
+added debug mode for zbx_parse_iostat_values.sh scripts
 
 
 Requirements
--- zabbix-templates/linux-disk-io-stats/usr-local-bin/zbx_parse_iostat_values.sh
+++ zabbix-templates/linux-disk-io-stats/usr-local-bin/zbx_parse_iostat_values.sh
@@ -2,17 +2,47 @@
 
 dev=$1
 
-#disk=mpathap3
+type=$2
+
+#enable debug mode
+debug=0
 
 if [[ -z "$dev" ]]; then
-  echo "error: wrong input value"
+  echo "error: wrong input value (device)"
   exit 1
 fi
 
-iostats=`iostat -xN |egrep -o "^${dev}[[:space:]]+.*"`
+if [[ -z "$type" ]]; then
+  echo "error: wrong input value (type)"
+  exit 1
+fi
+
+columns=`iostat -xN |egrep -o "^Device.*"`
+
+columnsarray=($columns)
+
+column_id=1
+
+for i in "${columnsarray[@]}"
+do
+        #echo "column: $i"
+
+        if [[ "$i" = "$type" ]]; then
+
+            if [[ $debug -eq 1 ]]; then
+                echo "right column (${i}) found...column_id: $column_id "
+            fi
+
+            id="$"
+            column_id_id=$id$column_id
+
+            iostats=`iostat -xN |egrep -o "^${dev}[[:space:]]+.*" |awk "{print ${column_id_id}}"`
+        fi
+    column_id=$[column_id + 1]
+done
 
 if [ -z "$iostats" ]; then
-    echo "error: device not found (${dev})"
+    echo "error: \"device\" or \"type\" not found (${dev},${type})"
     exit 3
 fi
 
@@ -25,5 +55,14 @@ fi
 
 echo $iostats
 
+if [[ $debug -eq 1 ]]; then
+    echo "- - - - - - - - - -"
+    echo $columns
+    iostats_debug=`iostat -xN |egrep -o "^${dev}[[:space:]]+.*"`
+    echo $iostats_debug
+    echo "- - - - - - - - - -"
+fi
+
 exit 0
+
 

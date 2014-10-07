bb28c7bce71788d199d026ca8a3fcae5c5cecd0b
Patrik Majer <p.majer@tcpisek.cz>
2014-10-07 17:54:29 +0200

    add Template SNMP Interfaces - Advanced

---
+++ zabbix-templates/snmp_interfaces_-_Advanced/docs-verification/example_run.md
@@ -0,0 +1,122 @@
+Example run
+=========
+
+Juniper switches in VC
+-------
+
+snmpwalk
+---------------
+```
+[root@monitoring-zabbix]# snmpwalk -c public -v1 172.x.0.111 .1.3.6.1.2.1.2.2.1
+IF-MIB::ifIndex.4 = INTEGER: 4
+IF-MIB::ifIndex.5 = INTEGER: 5
+IF-MIB::ifIndex.6 = INTEGER: 6
+IF-MIB::ifIndex.7 = INTEGER: 7
+.
+.
+.
+IF-MIB::ifIndex.538 = INTEGER: 538
+IF-MIB::ifIndex.540 = INTEGER: 540
+IF-MIB::ifIndex.542 = INTEGER: 542
+.
+.
+.
+IF-MIB::ifDescr.501 = STRING: ge-0/0/0
+IF-MIB::ifDescr.502 = STRING: ge-0/0/0.0
+IF-MIB::ifDescr.503 = STRING: ge-0/0/1
+IF-MIB::ifDescr.505 = STRING: ge-0/0/10
+IF-MIB::ifDescr.506 = STRING: ge-0/0/11
+.
+.
+.
+IF-MIB::ifOperStatus.10 = INTEGER: up(1)
+IF-MIB::ifOperStatus.11 = INTEGER: up(1)
+IF-MIB::ifOperStatus.12 = INTEGER: up(1)
+IF-MIB::ifOperStatus.21 = INTEGER: up(1)
+IF-MIB::ifOperStatus.33 = INTEGER: down(2)
+IF-MIB::ifOperStatus.34 = INTEGER: lowerLayerDown(7)
+IF-MIB::ifOperStatus.35 = INTEGER: down(2)
+IF-MIB::ifOperStatus.37 = INTEGER: up(1)
+IF-MIB::ifOperStatus.38 = INTEGER: up(1)
+IF-MIB::ifOperStatus.39 = INTEGER: down(2)
+IF-MIB::ifOperStatus.40 = INTEGER: down(2)
+IF-MIB::ifOperStatus.41 = INTEGER: down(2)
+.
+.
+.
+```
+
+snmp iface advanced discovery
+---------------
+
+```
+[root@monitoring-zabbix externalscripts]# ./snmp_iface_advanced_discovery.pl public 172.x.0.111 .1.3.6.1.2.1.2.2.1
+
+{
+        "data":[
+.
+.
+.
+        {
+                "{#STATUS}":"1",
+                "{#NAME}":"ipip",
+                "{#ID}":"9",
+                "{#ALL}":"-1--ipip--9-",
+        }
+        ,
+        {
+                "{#STATUS}":"2",
+                "{#NAME}":"ge-7/0/5",
+                "{#ID}":"802",
+                "{#ALL}":"-2--ge-7/0/5--802-",
+        }
+        ,
+        {
+                "{#STATUS}":"1",
+                "{#NAME}":"bme0.32768",
+                "{#ID}":"38",
+                "{#ALL}":"-1--bme0.32768--38-",
+        }
+        ,
+        {
+                "{#STATUS}":"1",
+                "{#NAME}":"lsi",
+                "{#ID}":"4",
+                "{#ALL}":"-1--lsi--4-",
+        }
+        ,
+        {
+                "{#STATUS}":"7",
+                "{#NAME}":"me0.0",
+                "{#ID}":"34",
+                "{#ALL}":"-7--me0.0--34-",
+        }
+        ,
+        {
+                "{#STATUS}":"2",
+                "{#NAME}":"ge-8/0/44",
+                "{#ID}":"745",
+                "{#ALL}":"-2--ge-8/0/44--745-",
+        }
+        ,
+.
+.
+.
+        ]
+}
+```
+
+License
+-------
+
+This template were distributed under GNU General Public License 2.
+
+### Copyright
+
+Copyright (c) 2014 Patrik Majer
+
+### Authors
+
+Patrik Majer
+      (patrik.majer.pisek |at| gmail |dot| com)
+      
---
+++ zabbix-templates/snmp_interfaces_-_Advanced/external-scripts/snmp_iface_advanced_discovery.pl
@@ -0,0 +1,132 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Net::SNMP v5.1.0 qw(:snmp DEBUG_ALL);
+use Getopt::Std;
+
+my $snmp_community = $ARGV[0];
+# ip
+my $ip = $ARGV[1];
+#snmp oid
+my $snmp_oid = $ARGV[2];
+#timeout for snmpwalk
+my $timeout = 5;
+
+if ( $#ARGV != 2 )
+{
+    print " Not enough parameters\n";
+    print " Usage: ibm_amm_snmpget.sh <SNMP_COMMUNITY> <IP> <OID>\n";
+    exit 2;
+}
+
+my ($s, $e) = Net::SNMP->session(
+   -hostname => $ip,
+   -version  => 1,
+   -community    =>  $snmp_community,
+#   exists($OPTS{a}) ? (-authprotocol =>  $OPTS{a}) : (),
+#   exists($OPTS{A}) ? (-authpassword =>  $OPTS{A}) : (),
+#   exists($OPTS{D}) ? (-domain       =>  $OPTS{D}) : (),
+#   exists($OPTS{d}) ? (-debug        => DEBUG_ALL) : (),
+#   exists($OPTS{m}) ? (-maxmsgsize   =>  $OPTS{m}) : (),
+#   exists($OPTS{r}) ? (-retries      =>  $OPTS{r}) : (),
+#   exists($OPTS{t}) ? (-timeout      =>  $OPTS{t}) : (),
+#   exists($OPTS{u}) ? (-username     =>  $OPTS{u}) : (),
+#   exists($OPTS{v}) ? (-version      =>  $OPTS{v}) : (),
+#   exists($OPTS{x}) ? (-privprotocol =>  $OPTS{x}) : (),
+ #  exists($OPTS{X}) ? (-privpassword =>  $OPTS{X}) : ()
+);
+
+# Was the session created?
+if (!defined($s)) {
+   _exit($e);
+}
+
+#
+# WALK
+#
+my @args = (-varbindlist    => [$snmp_oid.".1"] );
+
+my $oid;
+
+my %arr;
+
+while (defined($s->get_next_request(@args)))
+{
+  $oid = ($s->var_bind_names())[0];
+
+  if (!oid_base_match($snmp_oid.".1", $oid)) { last; }
+
+  # INDEX
+  my $id1 = $s->var_bind_list()->{$oid};
+  $arr{$id1}{"id"} = $id1;
+
+  # NAME
+  my $oid_name = $snmp_oid.".2.".$id1;
+  my $rs = $s->get_request(-varbindlist => [$oid_name],);
+
+  $arr{$id1}{"name"} = $rs->{$oid_name};
+
+#  print "DEBUG:\n oid: ". $oid . " \n oid_name: " .$oid_name . "\n name: " . $rs->{$oid_name} . "\n";
+
+  #OperStatus
+  my $oid_status = $snmp_oid.".8.".$id1;
+  my $rs2 = $s->get_request(-varbindlist => [$oid_status],);
+
+  $arr{$id1}{"status"} = $rs2->{$oid_status};
+
+ # print "DEBUG:\n oid: ". $oid . " \n oid_status: " .$oid_name . "\n status: " . $rs2->{$oid_status} . "\n";
+
+
+  @args = (-varbindlist => [$oid]);
+}
+
+#
+# PRINT RESULT
+#
+
+my $id;
+my $role;
+
+my $firstline;
+print "{\n";
+print "\t\"data\":[\n\n";
+
+for $id ( keys %arr) {
+
+  print "\t,\n" if not $firstline;
+  $firstline = 0;
+
+  print "\t{\n";
+
+  my $all = "";
+
+  for $role ( keys %{ $arr{$id} } ) {
+    print "\t\t\"{#".uc($role)."}\":\"" . $arr{$id}{$role} ."\",\n";
+    $all .= "-".$arr{$id}{$role}."-";
+  }
+  print "\t\t\"{#".uc("all")."}\":\"" . $all ."\",\n";
+
+  print "\t}\n";
+}
+
+print "\n\t]\n";
+print "}\n";
+
+#
+# END
+#
+$s->close();
+
+exit 0;
+
+#
+# [private functions]
+#
+sub _exit
+{
+   printf join('', sprintf("%s: ", "snmp_advaced_discovery.pl"), shift(@_), ".\n"), @_;
+   exit 1;
+}
+
